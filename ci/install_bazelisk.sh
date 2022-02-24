4caeb2d555fcb316032ede1d3bb47cf018693161
sakshisharmapuresoftware <53863764+sakshisharmapuresoftware@users.noreply.github.com>
2022-02-24 19:06:52 +0530

    Update install_bazelisk.sh

--- ci/install_bazelisk.sh
+++ ci/install_bazelisk.sh
@@ -7,5 +7,5 @@ set -e
 
 BAZELISK_VERSION=v1.10.1
 
-wget -O /usr/local/bin/bazel https://github.com/bazelbuild/bazelisk/releases/download/$BAZELISK_VERSION/bazelisk-linux-amd64
+wget -O /usr/local/bin/bazel https://github.com/bazelbuild/bazelisk/releases/download/$BAZELISK_VERSION/bazelisk-linux-arm64
 chmod +x /usr/local/bin/bazel
